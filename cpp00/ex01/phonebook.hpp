4472c958dbed8c0af56f73bf0039b235d0471b67
donghyun1998 <donghyun1998@naver.com>
2023-06-01 19:36:29 +0900

    add phone_book.hpp

--- cpp00/ex01/phonebook.hpp
+++ cpp00/ex01/phonebook.hpp
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   phonebook.hpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: donghyk2 <donghyk2@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/29 22:54:17 by donghyk2          #+#    #+#             */
-/*   Updated: 2023/06/01 19:34:53 by donghyk2         ###   ########.fr       */
+/*   Created: 2023/06/01 19:32:38 by donghyk2          #+#    #+#             */
+/*   Updated: 2023/06/01 19:34:32 by donghyk2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "phonebook.hpp"
+#ifndef PHONEBOOK_HPP
+# define PHONEBOOK_HPP
 
-int	main(void)
-{
-	
-}
+#include <iostream>
+
+#endif
