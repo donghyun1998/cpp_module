49e3bf43b7061555eda867471b6ee03b2525be5e
donghyun1998 <donghyun1998@naver.com>
2023-06-15 12:46:07 +0900

    change struct to class

--- cpp00/ex01/main.cpp
+++ cpp00/ex01/main.cpp
@@ -6,14 +6,14 @@
 /*   By: donghyk2 <donghyk2@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/29 22:54:17 by donghyk2          #+#    #+#             */
-/*   Updated: 2023/06/15 12:31:05 by donghyk2         ###   ########.fr       */
+/*   Updated: 2023/06/15 12:45:24 by donghyk2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string>
 
-struct t_info
+struct Contact
 {
 	std::string	first_name;
 	std::string	last_name;
@@ -24,10 +24,9 @@ struct t_info
 
 class PhoneBook {
 private:
-	t_info	info[8]; // new 안됨 할당X
+	Contact	info[8]; // new 안됨 할당X
 	int		lastIdx; // 추가 하면 넣을 인덱스
 	int		infoSize; // 사람 수만큼 빼주고 8로 나누면 시작 인덱스 완성
-	// 첫번째 인덱스는 |lastIdx - infoSize|
 public:
 
 	PhoneBook()
@@ -52,14 +51,12 @@ public:
 			infoSize++;
 		if (++lastIdx >= 8)
 			lastIdx %= 8;
-		std::cout << "infosize: " << infoSize << " lastidx: " << lastIdx << '\n'; ////////
 	}
 
 	void	PrintInfo(std::string s)
 	{
 		if (s.size() < 8)
 		{
-			// 출력하고 남은만큼 스페이스바
 			std::cout << s;
 			for (int i = 0; i < 8 - (int)s.size(); i++)
 				std::cout << ' ';
@@ -84,14 +81,14 @@ public:
 			if (infoSize < 8)
 				num = (lastIdx - infoSize + idx) % 8;
 			else
-				num = (lastIdx + 1 + idx) % 8; // 이거 아님 임시로 해둠
+				num = (lastIdx + 1 + idx) % 8;
 
 			std::cout << idx << "         |";
 			PrintInfo(info[num].first_name);
 			std::cout << '|';
 			PrintInfo(info[num].last_name);
 			std::cout << '|';
-			PrintInfo(info[num].nickname); // seg뜸
+			PrintInfo(info[num].nickname);
 			std::cout << '\n';
 			idx++;
 		}
@@ -109,19 +106,9 @@ int	main(void)
 		std::cout << "명령을 입력하세요\n";
 		std::cin >> cmd;
 		if (cmd == "ADD")
-		{
-			// 프로그램은 한 번에 한 필드씩, 모든 필드가 채워질 때까지 사용자에게 새 연락처의 정보를 적을 수 있도록 안내하여야 합니다.
-			// 전화번호부 (PhoneBook) 는 코드 내에서 클래스의 인스턴스로 정의되어야 하며, 연락처 배열을 포함하여야 합니다.
-			// 연락처는 반드시 코드 내부에서 클래스의 인스턴스로 정의되어야 합니다.
 			phoneBook.AddInfo();
-		}
 		else if (cmd == "SEARCH")
-		{
-			// 프로그램은 존재하는 (비어있지 않은) 모든 연락처 리스트를 다음과 같은 4개의 열로 나누어 보여주어야 합니다 : 인덱스, 성, 이름, 별명
-			// 각 열은 너비 10 문자로, 가로 정렬되어 있어야 하며 '|' 문자로 구분되어야 합니다. 열의 너비보다 긴 출력 문자열은 잘리고 마지막 표시 가능 문자는 점 ('.') 으로 치환됩니다.
-			// 그리고 프로그램은 원하는 연락처 항목의 인덱스를 입력받아, 연락처를 한 필드당 한 줄씩 출력하여야 합니다. 의미없는 입력값에 대해선 적절한 동작을 정의하세요.
 			phoneBook.Search();
-		}
 		else if (cmd == "EXIT")
 			break ;
 		else
