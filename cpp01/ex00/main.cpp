e98592a6d33c998c14be0fc9c3da69d1acdf4677
donghyun1998 <donghyun1998@naver.com>
2023-07-02 17:59:55 +0900

    start cpp01

---
+++ cpp01/ex00/main.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: donghyk2 <donghyk2@student.42.kr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/02 14:23:29 by donghyk2          #+#    #+#             */
+/*   Updated: 2023/07/02 15:09:00 by donghyk2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+class Zombie {
+private:
+	/* data */
+public:
+	Zombie(/* args */);
+	~Zombie();
+	void announce( void );
+};
+
+Zombie::Zombie(/* args */) {
+}
+
+Zombie::~Zombie() {
+}
+// Zombie* newZombie( std::string name );
+// void randomChump( std::string name );
+int	main(void) {
+
+}
