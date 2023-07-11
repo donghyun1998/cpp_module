07b02947922388302be8913575e77c1b182638ff
donghyun1998 <donghyun1998@naver.com>
2023-07-11 19:06:59 +0900

    ex00:: ok?

--- cpp02/ex00/Fixed.cpp
+++ cpp02/ex00/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: donghyk2 <donghyk2@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/05 20:28:54 by donghyk2          #+#    #+#             */
-/*   Updated: 2023/07/11 15:43:10 by donghyk2         ###   ########.fr       */
+/*   Updated: 2023/07/11 18:59:59 by donghyk2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,22 @@ Fixed::Fixed() {
 	std::cout << "Default constructor called" << std::endl;
 }
 
-Fixed::Fixed(const Fixed& obj) : {
+Fixed::Fixed(const Fixed& obj) {
+	std::cout << "Copy constructor called" << std::endl;
 	_raw_bits = obj.getRawBits();
-	std::cout << "Copy assignment operator called" << std::endl;
 }
+
+// Fixed::Fixed(const Fixed& obj) : _raw_bits(obj.getRawBits()){
+// 	std::cout << "Copy assignment operator called" << std::endl;
+// }
 
 Fixed::~Fixed() {
 	std::cout << "Destructor called" << std::endl;
 }
-
 Fixed& Fixed::operator=(const Fixed& obj) {
 	std::cout << "Copy assignment operator called" << std::endl;
+	this->_raw_bits = obj.getRawBits();
+	return (*this);
 }
 int	Fixed::getRawBits( void ) const {
 	std::cout << "getRawBits member function called" << std::endl;
