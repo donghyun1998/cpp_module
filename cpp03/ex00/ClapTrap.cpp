9fc95ff723bd6e382b288346b806a0137ac98c96
donghyun1998 <donghyun1998@naver.com>
2023-07-16 21:09:19 +0900

    ex00:: add claptrap

--- cpp03/ex00/ClapTrap.cpp
+++ cpp03/ex00/ClapTrap.cpp
@@ -6,16 +6,73 @@
 /*   By: donghyk2 <donghyk2@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/15 15:08:59 by donghyk2          #+#    #+#             */
-/*   Updated: 2023/07/15 15:14:05 by donghyk2         ###   ########.fr       */
+/*   Updated: 2023/07/16 21:07:49 by donghyk2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
 
-ClapTrap::ClapTrap() {}
-ClapTrap::ClapTrap(const ClapTrap& obj) {}
+std::string		ClapTrap::getName(void) {
+	return (this->_name);
+}
+unsigned int 	ClapTrap::getHp(void) {
+	return (this->_hp);
+}
+unsigned int 	ClapTrap::getEp(void) {
+	return (this->_ep);
+}
+unsigned int 	ClapTrap::getDp(void) {
+	return (this->_dp);
+}
+void			ClapTrap::setName(std::string s) {
+	this->_name = s;
+}
+void		 	ClapTrap::setHp(unsigned int num) {
+	this->_hp = num;
+}
+void		 	ClapTrap::setEp(unsigned int num) {
+	this->_ep = num;
+}
+void		 	ClapTrap::setDp(unsigned int num) {
+	this->_dp = num;
+}
+
+
+
+ClapTrap::ClapTrap() {
+	setName("null");
+	setHp(0);
+	setEp(0);
+	setDp(0);
+}
+
+ClapTrap::ClapTrap(const ClapTrap& obj) {
+	setName(obj._name);
+	setHp(obj._hp);
+	setEp(obj._ep);
+	setDp(obj._dp);
+}
+
 ClapTrap::~ClapTrap() {}
 ClapTrap& ClapTrap::operator=(const ClapTrap& obj) {}
-void ClapTrap::attack(const std::string& target) {}
-void ClapTrap::takeDamage(unsigned int amount) {}
-void ClapTrap::beRepaired(unsigned int amount) {}
+
+ClapTrap::ClapTrap(std::string name) {}
+
+void ClapTrap::attack(const std::string& target) {
+	std::cout << _name << " attacks "  << target << std::endl;
+	// ClapTrap <name> attacks <target>, causing <damage> points of damage!
+}
+
+void ClapTrap::takeDamage(unsigned int amount) {
+	// 체력 확인 해줘야함
+		std::cout << _name << " - " << amount << "hp" << std::endl;
+	if (this->_hp >= amount)
+		this->_hp -= amount;
+	else
+		this->_hp = 0; // 일단 0되게 함
+}
+
+void ClapTrap::beRepaired(unsigned int amount) {
+	std::cout << _name << " + " << amount << "hp" << std::endl;
+	this->_hp += amount;
+}
--- cpp03/ex00/ClapTrap.hpp
+++ cpp03/ex00/ClapTrap.hpp
@@ -6,28 +6,44 @@
 /*   By: donghyk2 <donghyk2@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/15 15:09:01 by donghyk2          #+#    #+#             */
-/*   Updated: 2023/07/15 15:14:22 by donghyk2         ###   ########.fr       */
+/*   Updated: 2023/07/16 21:03:08 by donghyk2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CLAPTRAP_HPP
 #define CLAPTRAP_HPP
-#define OK 0
-#define KO 1
+#define MAXHP 10
+#define MAXMP 10
 
 #include <string>
 #include <iostream>
+#include <string>
 
 class ClapTrap {
  private:
+	std::string		_name;
+	unsigned int	_hp;
+	unsigned int	_ep;
+	unsigned int	_dp;
  public:
 	ClapTrap();
 	ClapTrap(const ClapTrap& obj);
 	~ClapTrap();
 	ClapTrap& operator=(const ClapTrap& obj);
+
+	std::string		getName(void);
+	unsigned int 	getHp(void);
+	unsigned int 	getEp(void);
+	unsigned int 	getDp(void);
+	void			setName(std::string s);
+	void		 	setHp(unsigned int num);
+	void		 	setEp(unsigned int num);
+	void		 	setDp(unsigned int num);
+
+	ClapTrap(std::string name);
 	void attack(const std::string& target);
-	void takeDamage(unsigned int amount);
-	void beRepaired(unsigned int amount);
+	void takeDamage(unsigned int amount); // 딜 받았음
+	void beRepaired(unsigned int amount); // 수리 하다?
 };
 
 #endif
