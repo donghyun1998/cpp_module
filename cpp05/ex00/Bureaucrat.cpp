f6a026d95aaaf833d2ba15ce4b0e6d7bf89dd84d
donghyun1998 <donghyun1998@naver.com>
2023-08-07 14:53:47 +0900

    exception 조금 이해함

--- cpp05/ex00/Bureaucrat.cpp
+++ cpp05/ex00/Bureaucrat.cpp
@@ -6,34 +6,39 @@
 /*   By: donghyk2 <donghyk2@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/04 21:21:02 by donghyk2          #+#    #+#             */
-/*   Updated: 2023/08/04 21:42:44 by donghyk2         ###   ########.fr       */
+/*   Updated: 2023/08/07 14:53:14 by donghyk2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
-Bureaucrat::Bureaucrat() : name("null") {
-	grade = 150; // TODO: 초기화 150으로 되는거 맞는지
+const char* Bureaucrat::GradeTooHighException::what(void) const throw() {
+	return ("CustomException");
 }
-Bureaucrat::Bureaucrat(const Bureaucrat& obj) : name(obj.name) {
-	grade = obj.grade;
+const char* Bureaucrat::GradeTooLowException::what(void) const throw() {
+	return ("CustomException");
+}
+
+Bureaucrat::Bureaucrat() : name("null") {
+	this->grade = 150; // TODO: 초기화 150으로 되는거 맞는지
+}
+Bureaucrat::Bureaucrat(const Bureaucrat& obj) {
+	const_cast<std::string&>(this->name) = obj.name;
+	this->grade = obj.grade;
 }
 Bureaucrat::~Bureaucrat() {}
 Bureaucrat& Bureaucrat::operator=(const Bureaucrat& obj) {
-	// ??? const name 못 바꾸는디
-
+	const_cast<std::string&>(this->name) = obj.name;
+	this->grade = obj.grade;
 }
-void Bureaucrat::upgrade() {
+void Bureaucrat::upGrade() {
 	try {
 		this->grade -= 1;
 		if (this->grade < TOP)
 			throw (Bureaucrat::GradeTooHighException); // 어케하누
 	}
-	catch (const std::exception& e) {
+	catch (const std::exception& e) { // throw 조건문 안에는 throw된 것의 형? 이 오는듯??
 
 	}
 }
-void Bureaucrat::downgrade() {} // 이게 최선인가 1등급이 높아서 안 헷갈리게 이름 짓고싶은 {}
-
-void Bureaucrat::GradeTooHighException() {}
-void Bureaucrat::GradeTooLowException() {}
+void Bureaucrat::downGrade() {} // 이게 최선인가 1등급이 높아서 안 헷갈리게 이름 짓고싶은 {}
--- cpp05/ex00/Bureaucrat.hpp
+++ cpp05/ex00/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 /*   By: donghyk2 <donghyk2@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/03 02:13:39 by donghyk2          #+#    #+#             */
-/*   Updated: 2023/08/04 21:41:20 by donghyk2         ###   ########.fr       */
+/*   Updated: 2023/08/07 14:53:02 by donghyk2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,14 +24,21 @@ class Bureaucrat {
   const std::string	name;
   int	grade; // 구간 잘 체크해야할듯
  public:
+  class GradeTooHighException : public std::exception { // 이거 맞나???
+   public:
+    const char* what(void) const throw();
+  };
+  class GradeTooLowException : public std::exception { // 이거 맞나???
+   public:
+    const char* what(void) const throw();
+  };
+
   Bureaucrat();
   Bureaucrat(const Bureaucrat& obj);
   ~Bureaucrat();
   Bureaucrat& operator=(const Bureaucrat& obj);
-  void upgrade();
-  void downgrade(); // 이게 최선인가 1등급이 높아서 안 헷갈리게 이름 짓고싶은데
-  void GradeTooHighException();
-  void GradeTooLowException();
+  void upGrade();
+  void downGrade(); // 이게 최선인가 1등급이 높아서 안 헷갈리게 이름 짓고싶은데
 };
 
 #endif
