836ddeb1853dbbd9ddf842595a40f5a0f6ae3847
donghyun1998 <donghyun1998@naver.com>
2023-08-08 17:12:48 +0900

    try 문 안에 다 때려박음

--- cpp05/ex00/main.cpp
+++ cpp05/ex00/main.cpp
@@ -6,30 +6,26 @@
 /*   By: donghyk2 <donghyk2@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/04 21:25:22 by donghyk2          #+#    #+#             */
-/*   Updated: 2023/08/08 16:39:59 by donghyk2         ###   ########.fr       */
+/*   Updated: 2023/08/08 17:11:56 by donghyk2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
 int	main() {
-	Bureaucrat	a(150, "a");
-	Bureaucrat	b(1, "b");
+	try {
+		Bureaucrat	a(150, "a");
+		Bureaucrat	b(1, "b");
 
-	try {
+
 		a.downGrade();
-	}
-	catch(std::exception &e) {
-		std::cout << e.what() << std::endl;
-	}
-	a.upGrade();
-	std::cout << a << std::endl;
-	try {
+		a.upGrade();
+		std::cout << a << std::endl;
 		b.upGrade();
+		b.downGrade();
+		std::cout << b << std::endl;
 	}
 	catch (std::exception &e) {
 		std::cout << e.what() << std::endl;
 	}
-	b.downGrade();
-	std::cout << b << std::endl;
 }
