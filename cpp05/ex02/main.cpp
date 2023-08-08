ff851f1535dea41f31ea44e1e00d68871af4505a
donghyun1998 <donghyun1998@naver.com>
2023-08-09 00:04:02 +0900

    main form-> aform

--- cpp05/ex02/main.cpp
+++ cpp05/ex02/main.cpp
@@ -6,22 +6,18 @@
 /*   By: donghyk2 <donghyk2@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/04 21:25:22 by donghyk2          #+#    #+#             */
-/*   Updated: 2023/08/08 18:48:51 by donghyk2         ###   ########.fr       */
+/*   Updated: 2023/08/09 00:03:40 by donghyk2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
-#include "Form.hpp"
+#include "AForm.hpp"
 
 int	main() {
 	try {
-		Form fa("formA", false, 140, 150);
+		// AForm fa("formA", false, 140, 150);
 		Bureaucrat	a(150, "a");
 		Bureaucrat	b(140, "b");
-
-		std::cout << fa << std::endl;
-		fa.beSigned(a);
-		fa.beSigned(b);
 	}
 	catch (std::exception &e) {
 		std::cout << e.what() << std::endl;
