0043a94b5528498aa29c69ff340bc6f3d4f4f261
donghyun1998 <donghyun1998@naver.com>
2023-08-13 14:32:46 +0900

    explicit 개뻘짓 함 일반 함수 말고 생성자에만 사용 가능함

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -6,22 +6,44 @@
 /*   By: donghyk2 <donghyk2@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/10 18:25:32 by donghyk2          #+#    #+#             */
-/*   Updated: 2023/08/13 13:52:38 by donghyk2         ###   ########.fr       */
+/*   Updated: 2023/08/13 14:28:02 by donghyk2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
 double ScalarConverter::_value;
 
-void	ScalarConverter::printChar() {
+static	double	changeStringToDouble(const std::string& input) {
+	const char* cStr = input.c_str();
+    return (std::atof(cStr));
+}
+char changeDoubleToChar(double value) {
+	return (static_cast<char>(value));
+}
+int changeDoubleToInt(double value) {
+	return (static_cast<int>(value));
+}
+float changeDoubleToFloat(double value) {
+	return (static_cast<float>(value));
+}
+double changeDoubleToDouble(double value) {
+	return (static_cast<double>(value));
+}
+void	ScalarConverter::printChar(const char c) {
 	if (std::isnan(_value) || std::isinf(_value))
 		std::cout << "char: impossible" << std::endl;
 	else if (!std::isprint(_value))
 		std::cout << "char: Non displayable" << std::endl;
 	else if (std::isprint(_value))
-		std::cout << "char: \'" << static_cast<char>(_value) << '\'' << std::endl;
+		std::cout << "char: \'" << c << '\'' << std::endl;
 }
-// void	ScalarConverter::printInt(const std::string& input) {}
+void	ScalarConverter::printInt(const int d) {
+	if (std::isnan(_value) || std::isinf(_value))
+		std::cout << "int: impossible" << std::endl;
+	else
+		std::cout << "int: " << d << std::endl;
+	// TODO: int 에서 inf 어카지
+}
 // void	ScalarConverter::printFloat(const std::string& input) {}
 // void	ScalarConverter::printDouble(const std::string& input) {}
 
@@ -38,15 +60,11 @@ void	ScalarConverter::printChar() {
 // 	return (true);
 // } // 쓸모가 없누 어차피 유효한 리터럴만 들어온다고 써있음
 
-static	double	changeStringToDouble(const std::string& input) {
-	const char* cStr = input.c_str();
-    return (std::atof(cStr));
-}
 
 void ScalarConverter::convert(const std::string& input) {
 	_value = changeStringToDouble(input);
-	printChar();
-	// printInt(input);
+	printChar(changeDoubleToChar(_value));
+	printInt(changeDoubleToInt(_value));
 	// printFloat(input);
 	// printDouble(input);
 }
--- cpp06/ex00/ScalarConverter.hpp
+++ cpp06/ex00/ScalarConverter.hpp
@@ -6,7 +6,7 @@
 /*   By: donghyk2 <donghyk2@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/10 17:51:27 by donghyk2          #+#    #+#             */
-/*   Updated: 2023/08/13 13:46:52 by donghyk2         ###   ########.fr       */
+/*   Updated: 2023/08/13 14:28:14 by donghyk2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,9 +24,15 @@ class ScalarConverter { // static class는 static 멤버 변수함수 만 있어
   ScalarConverter& operator=(const ScalarConverter& obj);
 	// 이게 완벽한 static class아닐까
   static double	_value;
+
+  static explicit char changeDoubleToChar(double value);
+  static explicit int changeDoubleToInt(double value);
+  static explicit float changeDoubleToFloat(double value);
+  static explicit double changeDoubleToDouble(double value);
+
  public:
-  static void	printChar();
-//   static void	printInt(const std::string& input);
+  static void	printChar(const char c);
+  static void	printInt(const int d);
 //   static void	printFloat(const std::string& input);
 //   static void	printDouble(const std::string& input);
   static void convert(const std::string& input); //리터럴이라 const로받아야함
