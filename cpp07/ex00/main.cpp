97b0fe06f6b50b444ef6e0debd884fb17fd97abc
donghyun1998 <donghyun1998@naver.com>
2023-08-18 19:16:43 +0900

    잃어버린 내 1시간 되찾았다

--- cpp07/ex00/main.cpp
+++ cpp07/ex00/main.cpp
@@ -6,31 +6,11 @@
 /*   By: donghyk2 <donghyk2@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/17 15:46:23 by donghyk2          #+#    #+#             */
-/*   Updated: 2023/08/18 16:03:45 by donghyk2         ###   ########.fr       */
+/*   Updated: 2023/08/18 17:04:08 by donghyk2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "whatever.hpp"
-
-template <typename T>
-void	swap(T &a, T &b) {
-	T temp;
-	temp = a;
-	a = b;
-	b = temp;
-}
-template <typename T>
-T	min(T a, T b) {
-	if (a > b)
-		return (b);
-	return (a);
-}
-template <typename T>
-T	max(T a, T b) {
-	if (a > b)
-		return (a);
-	return (b);
-}
 
 int main( void ) {
 int a = 2;
--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: donghyk2 <donghyk2@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/18 16:14:19 by donghyk2          #+#    #+#             */
-/*   Updated: 2023/08/18 18:24:23 by donghyk2         ###   ########.fr       */
+/*   Updated: 2023/08/18 19:16:25 by donghyk2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,17 +21,57 @@ class Array {
  private:
   T* _array;
   std::size_t _size;
+  class Over_size : public std::exception {
+    virtual const char* what() const throw() {
+        return ("over size");
+    }
+  }
+  class Invalid_access : public std::exception {
+    virtual const char* what() const throw() {
+        return ("invalid access");
+    }
+  }
  public:
-  Array();
-  Array(unsigned int n);
-  Array(const Array& obj); // 깊은복사로 구현하래
-  ~Array();
-  Array& operator=(const Array& obj);
-  T& operator[](std::size_t obj);
-  std::size_t size() const;
+  Array() {
+    this->size = 0;
+    this->_array = NULL;
+  }
+  Array(unsigned int n) {
+    this->_size = n;
+    this->_array = new Array[n];
+  }
+  Array(const Array& obj) {
+    this->_size = obj._size;
+    this->_array = new Array[obj._size];
+    for (std::size_t i = 0; i < this->_size; i++)
+      this->_array[i] = obj._array[i];
+  }
+  ~Array() {
+    if (this->_array) {
+      delete[] this->_array;
+      this->_array = NULL;
+      this->_size = 0;
+    }
+  }
+  Array& operator=(const Array& obj) {
+    if (this->_array) {
+      delete[] this->_array;
+      this->_array = NULL;
+      this->_size = 0;
+    }
+    this = Array(obj); // 개깔끔
+    return (*this);
+  }
+  T& operator[](std::size_t obj) {
+    if (this->_array == NULL)
+      throw (Invalid_access());
+    if (this->_size >= obj._size)
+      throw (Over_size());
+    return (this._array[obj]);
+  }
+  std::size_t size() const {
+    return (this->_size);
+  }
 };
-
-
-#include "Array.tpp" //이렇게 마지막에 인클루드 해주는게 tpp
 
 #endif
