7bd5c32347b695c69eb611aefdcb1c92e565ce1f
donghyun1998 <donghyun1998@naver.com>
2023-08-19 15:51:16 +0900

    const는 왜 안되지??

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -6,19 +6,35 @@
 /*   By: donghyk2 <donghyk2@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/18 21:20:13 by donghyk2          #+#    #+#             */
-/*   Updated: 2023/08/18 23:49:29 by donghyk2         ###   ########.fr       */
+/*   Updated: 2023/08/19 15:50:22 by donghyk2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
+#include <algorithm>
+
 
 template <typename T>
-typename T::iterator	easyfind(T& obj, int num) {
+typename T::iterator	easyfind(T& container, int num) {
 	typename T::iterator iter;
-	iter = std::find(obj.begin)
-
-	iter
+	iter = container.begin();
+	while (iter != container.end()) {
+		if (*iter == num)
+			return (iter);
+		iter++;
+	}
+	return (iter);
 }
+// template <typename T>
+// typename T::iterator	easyfind(T& const container, int num) {
+// 	typename T::iterator iter;
+// 	iter = container.begin();
+// 	while (iter != container.end()) {
+// 		if (*iter == num)
+// 			return (iter);
+// 	}
+// 	return (iter);
+// }
 
 #endif
--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -6,12 +6,18 @@
 /*   By: donghyk2 <donghyk2@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/18 21:19:53 by donghyk2          #+#    #+#             */
-/*   Updated: 2023/08/18 21:20:08 by donghyk2         ###   ########.fr       */
+/*   Updated: 2023/08/19 15:49:48 by donghyk2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "easyfind.hpp"
+#include <iostream>
+#include <vector>
 
 int	main() {
-	
+	// std::vector<int>	v[] = {1, 2, 3, 4, 5};
+	std::vector<int>	v;
+	for (int i = 0; i < 5; i++)
+		v.push_back(i);
+	std::cout << *easyfind(v, 3) << std::endl;
 }
