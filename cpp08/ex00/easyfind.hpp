edc469c8103e60bc657e1a76ecb6f80ae5072021
donghyun1998 <donghyun1998@naver.com>
2023-08-21 00:39:22 +0900

    const 억까까지 처리 완료

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: donghyk2 <donghyk2@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/18 21:20:13 by donghyk2          #+#    #+#             */
-/*   Updated: 2023/08/19 21:05:02 by donghyk2         ###   ########.fr       */
+/*   Updated: 2023/08/21 00:38:32 by donghyk2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,39 +15,16 @@
 #include <algorithm>
 
 template <typename T>
-class stk :public std::stack {
-  private:
-	typename T::iterator iter;
-};
-
-
-template <typename T>
 typename T::iterator	easyfind(T& container, int num) {
 	typename T::iterator iter;
-	iter = std::find(T.begin(), T.end(), num); //아이 및친 순회하면 ko라네
+	iter = std::find(container.begin(), container.end(), num); //순회하면 koㄷㄷㄷ
 	return (iter);
 }
-
-// template <typename T> // 안되자나 시팔
-// typename T::container_type::iterator easyfind(T& container, int num) {
-//     typename T::container_type::iterator iter = container._Get_container().begin();
-//     while (iter != container._Get_container().end()) {
-//         if (*iter == num)
-//             return iter;
-//         iter++;
-//     }
-//     return iter;
-// }
-
-// template <typename T>
-// typename T::iterator	easyfind(const T& container, int num) {
-// 	typename T::iterator iter;
-// 	iter = container.begin();
-// 	while (iter != container.end()) {
-// 		if (*iter == num)
-// 			return (iter);
-// 	}
-// 	return (iter); // 걍  end 반환하게 함 map 처럼
-// }
+template <typename T>
+typename T::iterator	easyfind(const T& container, int num) {
+	typename T::iterator iter;
+	iter = std::find(container.begin(), container.end(), num);
+	return (iter);
+}
 
 #endif
--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: donghyk2 <donghyk2@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/18 21:19:53 by donghyk2          #+#    #+#             */
-/*   Updated: 2023/08/19 20:18:35 by donghyk2         ###   ########.fr       */
+/*   Updated: 2023/08/21 00:35:24 by donghyk2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,23 +20,14 @@
 
 int	main() {
 	std::vector<int>	v;
-	std::queue<int>		q;
-	std::stack<int>		stk;
 	std::deque<int>		deq;
 	std::list<int>		lst;
-	std::priority_queue<int>	pq;
 	for (int i = 0; i < 5; i++) {
 		v.push_back(i);
-		q.push(i);
-		stk.push(i);
 		deq.push_back(i);
 		lst.push_back(i);
-		pq.push(i);
 	}
 	std::cout << *easyfind(v, 3) << std::endl;
-	std::cout << *easyfind(q, 3) << std::endl;
-	std::cout << *easyfind(stk, 3) << std::endl;
 	std::cout << *easyfind(deq, 3) << std::endl;
 	std::cout << *easyfind(lst, 3) << std::endl;
-	std::cout << *easyfind(pq, 3) << std::endl;
 }
