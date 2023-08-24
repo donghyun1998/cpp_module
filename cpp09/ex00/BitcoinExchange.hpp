07dd74ee8a89293c91d4928a3d47df4fc4d3b068
donghyun1998 <donghyun1998@naver.com>
2023-08-24 23:55:39 +0900

    go to private

--- cpp09/ex00/BitcoinExchange.hpp
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: donghyk2 <donghyk2@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/22 17:29:27 by donghyk2          #+#    #+#             */
-/*   Updated: 2023/08/23 20:17:51 by donghyk2         ###   ########.fr       */
+/*   Updated: 2023/08/24 23:54:53 by donghyk2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,19 +21,19 @@
 class BitcoinExchange {
  private:
   std::map<std::string, double>  _db;
- public:
+  void	parseDb(const char* dbFileName);
+  void	addLineToMap(std::string line, std::fstream& dbFile);
+  void parseInput(char* inputFileName);
+  void  printOutputByLine(std::string line);
   class WrongDb : public std::exception {
 	  virtual const char* what() const throw();
   };
   class WrongInput : public std::exception {
 	  virtual const char* what() const throw();
   };
+ public:
 
   BitcoinExchange(const char* dbFileName, char* inputFileName);
-  void	parseDb(const char* dbFileName);
-  void	addLineToMap(std::string line, std::fstream& dbFile);
-  void parseInput(char* inputFileName);
-  void  printOutputByLine(std::string line);
 };
 
 #endif
