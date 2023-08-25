2bb94e34dfb96cfb7019b6b0c30b109ae5462c7f
donghyun1998 <donghyun1998@naver.com>
2023-08-25 16:42:45 +0900

    01 ok

--- cpp09/ex01/RPN.cpp
+++ cpp09/ex01/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: donghyk2 <donghyk2@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/25 00:34:47 by donghyk2          #+#    #+#             */
-/*   Updated: 2023/08/25 01:07:40 by donghyk2         ###   ########.fr       */
+/*   Updated: 2023/08/25 16:42:22 by donghyk2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,87 @@ const char* RPN::error::what() const throw() {
 	return ("error");
 }
 
-static bool isNumeric(const std::string& str) {
-	for (int i = 0; i < str.size(); i++)
-		if (!std::isdigit(str[i]))
-			return false;
-	return true;
+static bool isOperator(char c) {
+	if (c == '+' || c == '-' || c == '*' || c == '/')
+		return (true);
+	return (false);
+}
+
+void RPN::checkInput(std::string input) {
+	int numCnt = 0, operCnt = 0;
+
+	if (!std::isdigit(input[0]) || input[1] != ' ' || input.size() % 2 == 0)
+		throw (error());
+	for (std::size_t i = 1; i + 1 < input.size(); i += 2) {
+		if (input[i] != ' ')
+			throw (error());
+		if (std::isdigit(input[i + 1]))
+			numCnt++;
+		else if (isOperator(input[i + 1]))
+			operCnt++;
+		else
+			throw (error());
+	}
+	if (numCnt != operCnt)
+		throw (error());
+}
+
+void  RPN::plus() {
+	int b = _s.top();
+	_s.pop();
+	int	a = _s.top();
+	_s.pop();
+	_s.push(a + b);
+}
+void  RPN::minus() {
+	int b = _s.top();
+	_s.pop();
+	int	a = _s.top();
+	_s.pop();
+	_s.push(a - b);
+}
+void  RPN::multiply() {
+	int b = _s.top();
+	_s.pop();
+	int	a = _s.top();
+	_s.pop();
+	_s.push(a * b);
+}
+void  RPN::devide() {
+	int b = _s.top();
+	_s.pop();
+	int	a = _s.top();
+	_s.pop();
+	_s.push(a / b);
+}
+
+void	RPN::calculate(std::string input, int i) {
+	switch (input[i]) {
+		case '+' :
+			plus();
+			break ;
+		case '-' :
+			minus();
+			break ;
+		case '*' :
+			multiply();
+			break ;
+		case '/' :
+			devide();
+			break ;
+		default : // 숫자 들어온겨
+			_s.push(input[i] - '0');
+			break ;
+	}
 }
 
 RPN::RPN(std::string input) {
 	//  "8 9 * 9 - 9 - 9 - 4 - 1 +" 숫자 그대로 집어넣고 맨 위에 두개로 들어온 연산 수행
 	try {
-		getUnit
-		std::size_t	spaceIdx = input.find(' '); // 이거 startidx 늘러가면서 space찾는 함수 만들자
-		std::string baseNumString = input.substr(0, spaceIdx);
-		if (isNumeric(baseNumString) == false)
-			throw (error());
+		checkInput(input);
+		for (std::size_t i = 0; i < input.size(); i += 2)
+			calculate(input, i);
+		std::cout << _s.top() << std::endl;
 	}
 	catch (std::exception &e) {
 		std::cout << e.what() << std::endl;
--- cpp09/ex01/RPN.hpp
+++ cpp09/ex01/RPN.hpp
@@ -6,7 +6,7 @@
 /*   By: donghyk2 <donghyk2@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/23 20:33:45 by donghyk2          #+#    #+#             */
-/*   Updated: 2023/08/25 00:53:44 by donghyk2         ###   ########.fr       */
+/*   Updated: 2023/08/25 16:41:48 by donghyk2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,19 @@
 
 class RPN {
  private:
-  class error : std::exception {
+  class error : public std::exception {
     virtual const char* what() const throw();
   };
+  std::stack<int> _s;
+  // std::string _input;
+
+  std::string	getToken(int startIdx);
+  void  checkInput(std::string input);
+  void	calculate(std::string input, int i);
+  void  plus();
+  void  minus();
+  void  multiply();
+  void  devide();
  public:
   RPN(std::string input);
 };
