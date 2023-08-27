9273875b2ecdde264e65c80c5a429e1cc03a2aad
donghyun1998 <donghyun1998@naver.com>
2023-08-27 22:43:33 +0900

    최소한의 예외처리 완

--- cpp09/ex02/PmergeMe.hpp
+++ cpp09/ex02/PmergeMe.hpp
@@ -6,27 +6,31 @@
 /*   By: donghyk2 <donghyk2@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/25 16:46:17 by donghyk2          #+#    #+#             */
-/*   Updated: 2023/08/25 20:05:16 by donghyk2         ###   ########.fr       */
+/*   Updated: 2023/08/27 22:42:34 by donghyk2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PMERGEME_HPP
 #define PMERGEME_HPP
 #include <vector>
+#include <iostream>
+#include <exception>
 
 class PmergeMe {
  private:
-  std::vector<std::pair<int, int> > _v;
- public:
-  PmergeMe();
+  class InputError : public std::exception {
+    virtual const char* what() const throw();
+  };
+
+  std::vector<std::pair<int, int> > _input;
+
   PmergeMe(const PmergeMe& obj);
-  ~PmergeMe();
+  PmergeMe();
   PmergeMe& operator=(const PmergeMe& obj);
+ public:
+  ~PmergeMe();
+  PmergeMe(std::vector<int> input);
 };
 
 #endif
 
-PmergeMe::PmergeMe() {}
-PmergeMe::PmergeMe(const PmergeMe& obj) {}
-PmergeMe::~PmergeMe() {}
-PmergeMe& PmergeMe::operator=(const PmergeMe& obj) {}
--- cpp09/ex02/main.cpp
+++ cpp09/ex02/main.cpp
@@ -6,12 +6,37 @@
 /*   By: donghyk2 <donghyk2@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/25 16:46:07 by donghyk2          #+#    #+#             */
-/*   Updated: 2023/08/25 19:58:35 by donghyk2         ###   ########.fr       */
+/*   Updated: 2023/08/27 22:42:20 by donghyk2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
+#include <cstdlib>
+
+ // TODO: `shuf -i 1-100000 -n 3000 | tr "\n" " "` 이거 뭐 어케하는겨
+
+static bool	checkInput(int argc, char** argv) {
+	// TODO: "3 4 5 6" 이런거도 해야하나?
+	for (int i = 1; i < argc; i++) {
+		for (int j = 0; argv[i][j]; j++)
+			if (!std::isdigit(argv[i][j]))
+				return (false);
+	}
+	return (true);
+}
+
+static std::vector<int>	getVectorInput(int argc, char** argv) {
+	std::vector<int>	res;
+
+	for (int i = 1; i < argc; i++)
+		res.push_back(std::atoi(argv[i]));
+	return (res);
+}
 
 int	main(int argc, char** argv) {
-	
+	if (argc == 1 || checkInput(argc, argv) == false) {
+		std::cout << "wrong arg" << std::endl;
+		return 0;
+	}
+	PmergeMe(getVectorInput(argc, argv));
 }
