815b1b934ce6af80db290f829b232ecfaf8c98c5
donghyun1998 <donghyun1998@naver.com>
2023-08-27 22:58:48 +0900

    add TODO

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: donghyk2 <donghyk2@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/26 00:23:49 by donghyk2          #+#    #+#             */
-/*   Updated: 2023/08/27 22:43:13 by donghyk2         ###   ########.fr       */
+/*   Updated: 2023/08/27 22:58:33 by donghyk2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,5 @@
 PmergeMe::~PmergeMe() {}
 
 PmergeMe::PmergeMe(std::vector<int>	input) {
-	
+	// TODO: 시간 시작하기, 홀수 어떻게 뺄지, 굳이 페어로 안받고 그대로 main, sub 나눠도 될지
 }
--- cpp09/ex02/PmergeMe.hpp
+++ cpp09/ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: donghyk2 <donghyk2@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/25 16:46:17 by donghyk2          #+#    #+#             */
-/*   Updated: 2023/08/27 22:42:34 by donghyk2         ###   ########.fr       */
+/*   Updated: 2023/08/27 22:49:31 by donghyk2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,15 @@
 #include <vector>
 #include <iostream>
 #include <exception>
-
+ // cmd + 1, 2하면 화면 분할 했을때 넘어갈 수 있음 개꿀
 class PmergeMe {
  private:
-  class InputError : public std::exception {
+  class Error : public std::exception {
     virtual const char* what() const throw();
   };
 
-  std::vector<std::pair<int, int> > _input;
+  std::vector<int>  _main;
+  std::vector<int>  _sub;
 
   PmergeMe(const PmergeMe& obj);
   PmergeMe();
