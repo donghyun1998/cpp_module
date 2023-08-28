ae92089e79d6919331bb2d9c973b852eb1548af8
donghyun1998 <donghyun1998@naver.com>
2023-08-28 23:50:57 +0900

    add makeSortedPair

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: donghyk2 <donghyk2@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/26 00:23:49 by donghyk2          #+#    #+#             */
-/*   Updated: 2023/08/27 22:58:33 by donghyk2         ###   ########.fr       */
+/*   Updated: 2023/08/28 23:50:31 by donghyk2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,13 @@
 
 PmergeMe::~PmergeMe() {}
 
+void  PmergeMe::makeSortedPair(std::vector<int> input) {
+	for () // 홀수 어카지...?
+}
+
+
 PmergeMe::PmergeMe(std::vector<int>	input) {
 	// TODO: 시간 시작하기, 홀수 어떻게 뺄지, 굳이 페어로 안받고 그대로 main, sub 나눠도 될지
+
+
 }
--- cpp09/ex02/PmergeMe.hpp
+++ cpp09/ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: donghyk2 <donghyk2@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/25 16:46:17 by donghyk2          #+#    #+#             */
-/*   Updated: 2023/08/27 23:07:43 by donghyk2         ###   ########.fr       */
+/*   Updated: 2023/08/28 22:08:06 by donghyk2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,15 @@ class PmergeMe {
   class Error : public std::exception {
     virtual const char* what() const throw();
   };
-
+  std::vector<int, int> pairInput;
   std::vector<int>  _main;
   std::vector<int>  _sub;
 
   PmergeMe(const PmergeMe& obj);
   PmergeMe& operator=(const PmergeMe& obj);
+
+  void  makeSortedPair(std::vector<int> input);
+
  public:
   PmergeMe(); // 이거 왜 프라이빗이면 오류나지 전엔 안이랬는데
   ~PmergeMe();
