91c6d43c3f3d5d98aacf6821ca6e50cf8c23943d
donghyun1998 <donghyun1998@naver.com>
2023-09-04 20:37:14 +0900

    지옥에서 울부짖는 포드존슨

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: donghyk2 <donghyk2@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/26 00:23:49 by donghyk2          #+#    #+#             */
-/*   Updated: 2023/08/30 20:22:16 by donghyk2         ###   ########.fr       */
+/*   Updated: 2023/09/04 20:36:55 by donghyk2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,49 +14,89 @@
 
 PmergeMe::~PmergeMe() {}
 
-static bool	comparePairByFirst(std::pair< int, int> a, std::pair< int, int> b) {
-	return (a.first < b.first);
+static void ftInsertionSort(std::vector<std::pair< int, int> > &vec) {
+	int n = vec.size();
+	for (int i = 1; i < n; ++i) {
+		std::pair<int, int> key = vec[i];
+		int j = i - 1;
+
+		while (j >= 0 && vec[j].first > key.first) {
+			vec[j + 1] = vec[j];
+			j--;
+		}
+		vec[j + 1] = key;
+	}
 }
 
 void  PmergeMe::makeSortedPair(std::vector<int> input) {
 	if (input.size() % 2) {
-		this->oddFlag = true;
+		this->_oddFlag = true;
 		for (int i = 0; i < input.size() - 2; i += 2) {
-			pairInput.push_back(std::make_pair(std::max(input[i],input[i + 1])
+			_pairInput.push_back(std::make_pair(std::max(input[i],input[i + 1])
 				, std::min(input[i],input[i + 1])));
 		}
-		this->pairInput.push_back(std::make_pair(INT_MAX,input[input.size() - 1]));
+		this->_remain=  input[input.size() - 1];
 	}
 	else {
-		this->oddFlag = false;
+		this->_oddFlag = false;
 		for (int i = 0; i < input.size() - 1; i += 2) {
-			pairInput.push_back(std::make_pair(std::max(input[i],input[i + 1])
+			_pairInput.push_back(std::make_pair(std::max(input[i],input[i + 1])
 				, std::min(input[i],input[i + 1])));
 		}
 	}
-	std::sort(this->pairInput.begin(), this->pairInput.end(), comparePairByFirst);
+	ftInsertionSort(this->_pairInput);
+
+	for (int i = 0; i < _pairInput.size(); i++) {
+		this->_a.push_back(this->_pairInput[i].first);
+		this->_b.push_back(this->_pairInput[i].second);
+	}
 }
 
 
-/*
-main  1 2 4 6 8
-pending 1 3 5 7
-start end 찾아야함
-땡겨야 하는데...?
+void	PmergeMe::makeMainIdxArr() {
+	for (int i = 0; i < this->_pairInput.size(); i++)
+		this->_mainIdxArr.push_back(i);
+}
 
-2,1 4,3 6,5 8,7
- 이건 쓰레기다
+// int	isJacob[3001] = { 0 };
 
-*/
+void PmergeMe::fillIsJacob() {
+	int jacob[] = {2, 4, 10, 20, 42, 84, 170, 340, 682, 1364};
+	// idx라 하나씩 뺴줬음
+	for (int i = 0; i < this->_b.size(); i++)
+		this->_isJacob.push_back(0);
+	for (int i = 0; i < 9; i++)
+		this->_isJacob[jacob[i]] = jacob[i + 1];
+	this->_isJacob[jacob[9]] = this->_b.size() - 1;
+}
 
+int	PmergeMe::getNextJacobSthalNum(int prevIdx) {
+	if (this->_isJacob[prevIdx] != 0)
+		return (this->_isJacob[prevIdx]);
+}
 
-void	PmergeMe::makeMainIdxMap() {
-	this->_mainIdxMap =
+void PmergeMe::pendingBtoA() {
+	int idx = 0;
+	fillIsJacob();
+
+	while (idx < this->_b.size()) {
+		// 이분탐색 하면됨
+		// 넣은 a의 idx 이상인 mainIdxArr 1씩 더해줌
+
+		int nextIdx = getNextJacobSthalNum(idx);
+		if (nextIdx != 0)
+			idx = nextIdx;
+		else
+			idx--;
+	}
+
 }
 
 
 PmergeMe::PmergeMe(std::vector<int>	input) {
 	// TODO: 시간 시작하기
 	makeSortedPair(input);
-	makeMainIdxMap();
+	makeMainIdxArr();
+	pendingBtoA();
+	// _a 출력해주면 끝
 }
--- cpp09/ex02/PmergeMe.hpp
+++ cpp09/ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: donghyk2 <donghyk2@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/25 16:46:17 by donghyk2          #+#    #+#             */
-/*   Updated: 2023/08/30 16:39:03 by donghyk2         ###   ########.fr       */
+/*   Updated: 2023/09/04 20:31:02 by donghyk2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,19 +25,23 @@ class PmergeMe {
   class Error : public std::exception {
     virtual const char* what() const throw();
   };
-  std::vector<std::pair< int, int> > pairInput;
-  std::vector<int>  _main;
-  std::vector<int>  _pending;
-  std::vector<int>  _mainIdxMap;
-  bool  oddFlag; // TODO: 마지막에 intmax하나 빼줘야함, 중복은 어떻게 할지
+  std::vector<std::pair< int, int> > _pairInput;
+  std::vector<int>  _a; // mainchain
+  std::vector<int>  _b; // pending
+  std::vector<int>  _mainIdxArr;
+  std::vector<int>  _isJacob;
+  bool  _oddFlag;
+  int  _remain; // TODO: 마지막에 intmax하나 빼줘야함, 중복은 어떻게 할지
 
   PmergeMe(const PmergeMe& obj);
   PmergeMe& operator=(const PmergeMe& obj);
   PmergeMe();
 
   void  makeSortedPair(std::vector<int> input);
-  void  makeMainIdxMap();
-
+  void  makeMainIdxArr();
+  void  pendingBtoA();
+  void fillIsJacob();
+  int getNextJacobSthalNum(int prevIdx);
  public:
   ~PmergeMe();
   PmergeMe(std::vector<int> input);
