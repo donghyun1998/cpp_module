263dd78bf5d54ec7395551fc2d4936d5ad609666
donghyun1998 <donghyun1998@naver.com>
2023-09-14 14:30:37 +0900

    0, 정렬되어있는 인자 검사 추가

--- cpp09/ex02/main.cpp
+++ cpp09/ex02/main.cpp
@@ -6,20 +6,17 @@
 /*   By: donghyk2 <donghyk2@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/25 16:46:07 by donghyk2          #+#    #+#             */
-/*   Updated: 2023/09/13 16:50:45 by donghyk2         ###   ########.fr       */
+/*   Updated: 2023/09/14 14:29:02 by donghyk2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
 #include <cstdlib>
 
- // TODO: `shuf -i 1-100000 -n 3000 | tr "\n" " "` 이거 뭐 어케하는겨
-
 static bool	checkInput(int argc, char** argv) {
-	// TODO: "3 4 5 6" 이런거도 해야하나?
 	for (int i = 1; i < argc; i++) {
 		for (int j = 0; argv[i][j]; j++)
-			if (!std::isdigit(argv[i][j] || )) // ㅇㅣ미 정정렬렬된지
+			if (!std::isdigit(argv[i][j]) || argv[i][j] == '0')
 				return (false);
 	}
 	return (true);
@@ -31,6 +28,14 @@ static std::vector<int>	getVectorInput(int argc, char** argv) {
 	for (int i = 1; i < argc; i++)
 		res.push_back(std::atoi(argv[i]));
 	return (res);
+}
+
+static bool isSorted(std::vector<int> v) {
+	for (std::size_t i = 0; i < v.size() - 1; i++)
+		for (std::size_t j = i + 1; j < v.size(); j++)
+			if (v[i] > v[j])
+				return (false);
+	return (true);
 }
 
 static std::deque<int>	getDequeInput(int argc, char** argv) {
@@ -47,6 +52,10 @@ int	main(int argc, char** argv) {
 		return (0);
 	}
 	std::vector<int> vInput = getVectorInput(argc, argv);
+	if (isSorted(vInput)) {
+		std::cout << "wrong arg" << std::endl;
+		return (0);
+	}
 	std::deque<int> dInput = getDequeInput(argc, argv);
 	PmergeMe pmergeme(vInput, dInput);
 }
---
+++ cpp09/ex02/mergeinsertsort.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+static void sortVector(int size, std::vector<int>& origin, std::vector<int>& temp)
+{
+	if (size < 2)
+		return;
+	int remain = -1;
+	int mid = size / 2;
+	std::vector<int> pairIndex(origin.size(), -1);
+	for (int i = 0; i < mid; ++i)
+	{
+		if (origin[temp[i]] < origin[temp[i + mid]])
+			std::swap(temp[i], temp[i + mid]);
+		pairIndex[temp[i]] = temp[i + mid];
+	}
+	if (size & 1)
+		remain = temp[size - 1];
+	sortVector(size / 2, origin, temp);
+
+	std::vector<int> unsorted;
+	for (int i = 0; i < mid; ++i)
+		unsorted.push_back(pairIndex[temp[i]]);
+	temp.insert(temp.begin(), *unsorted.begin());
+	unsorted.erase(unsorted.begin());
+
+	int n = unsorted.size();
+  // 현재까지 정렬된 갯수
+	int sortedCnt = (size / 2) + 1;
+  // 정렬 후보군 중 정렬 시작 값의 위치
+	int prevStart = 0;
+  // jacob 계산 시작 인덱스
+	int i = 1;
+  // 정렬 후보군 중 현 step에서 정렬 할 갯수
+  // 2 2 6 10
+  // (3, 2), (5, 4), (11, 10, 9, 8, 7, 6)
+	int curr = 2;
+	while (prevStart < n)
+	{
+    //정렬 후보군 중 정렬될 마지막 값의 위치
+		int j = std::min(prevStart + curr - 1, n - 1);
+    // 마지막 값부터 넣어야 최적화 (최소 비교 가능)
+		while (j >= prevStart)
+		{
+			std::vector<int>::iterator low;
+			std::vector<int>::iterator start = temp.begin();
+			std::vector<int>::iterator end = start + std::min((1 << (i + 1)) - 1, sortedCnt);
+			MyCompareVector com(origin);
+			low = std::lower_bound(start, end, unsorted[j], com);
+			temp.insert(low, unsorted[j]);
+			++sortedCnt;
+			--j;
+		}
+		prevStart = std::min(prevStart + curr, n);
+		curr = ((1 << (i + 1)) - curr);
+		++i;
+	}
+	if (remain != -1)
+	{
+		std::vector<int>::iterator low;
+		std::vector<int>::iterator start = temp.begin();
+		std::vector<int>::iterator end = start + (size - 1);
+		MyCompareVector com(origin);
+		low = std::lower_bound(start, end, remain, com);
+		temp.insert(low, remain);
+	}
+}
+
+
+std::vector<int>	mergeInsertionSortInVector(std::vector<int>& input)
+{
+	std::vector<int> temp;
+	std::vector<int> ret;
+	int size = input.size();
+
+	for (int i = 0; i < size; ++i)
+		temp.push_back(i);
+
+	sortVector(size, input, temp);
+
+	for (int i = 0; i < size; ++i)
+		ret.push_back(input[temp[i]]);
+
+	return ret;
+}
